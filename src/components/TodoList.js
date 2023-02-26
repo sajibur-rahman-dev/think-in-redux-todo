582f686be6a4aed322f87e3df5a99b3896ecda0e
sajibur-rahman-dev <sajibur.dev@gmail.com>
2023-02-26 16:14:16 +0600

    filter by priorities is done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,27 @@ import TodoItem from "./TodoItem";
 
 export default function TodoList() {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filter);
+
   console.log(todos);
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {todos.map((todo) => (
+      {todos.filter(todo => {
+        switch (filters.status) {
+          case 'Complete':
+            return todo.completed;
+          case 'Incomplete':
+              return !todo.completed;
+          default:
+            return true;
+        }
+      }).filter((todo) => {
+        const colors = filters.colors;
+        if(colors.length > 0){
+          return colors.includes(todo?.priority);
+        }
+        return true;
+      }).map((todo) => (
         <TodoItem todo={todo} />
       ))}
     </div>
